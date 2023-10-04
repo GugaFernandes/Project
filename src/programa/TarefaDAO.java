e2fcaf60fc8d3c72060ab69e761bdd603948d113
Gustavo Germano Fernandes <gusgermes@gmail.com>
2023-10-04 15:01:37 -0300

    1

--- src/programa/TarefaDAO.java
+++ src/programa/TarefaDAO.java
@@ -1,5 +1,41 @@
 package programa;
+	
+	import java.util.ArrayList;
 
-public class TarefaDAO {
+	public class TarefaDAO {
+	    private ArrayList<Tarefa> tarefas = new ArrayList<>();
 
+	    public void criarTarefa(Tarefa tarefa) {
+	        tarefas.add(tarefa);
+	    }
+
+	    public void removerTarefa(String titulo) {
+	        Tarefa tarefaParaRemover = buscarTarefaPorTitulo(titulo);
+	        if (tarefaParaRemover != null) {
+	            tarefas.remove(tarefaParaRemover);
+	        }
+	    }
+
+	    public ArrayList<Tarefa> listarTarefas() {
+	        return tarefas;
+	    }
+
+	    public Tarefa buscarTarefaPorTitulo(String titulo) {
+	        for (Tarefa tarefa : tarefas) {
+	            if (tarefa.getTitulo().equalsIgnoreCase(titulo)) {
+	                return tarefa;
+	            }
+	        }
+	        return null;
+	    }
+
+	    public void atualizarTarefa(Tarefa tarefa, String novaDescricao, String novaDataVencimento) {
+	        if (novaDescricao != null && !novaDescricao.isEmpty()) {
+	            tarefa.setDescricao(novaDescricao);
+	        }
+	        if (novaDataVencimento != null && !novaDataVencimento.isEmpty()) {
+	            tarefa.setDataVencimento(novaDataVencimento);
+	        }
+	    }
+	}
 }
