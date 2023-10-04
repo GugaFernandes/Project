1cfcd0f7a89e16ddc7df8272b5c3c5c8219e3be4
Gustavo Germano Fernandes <gusgermes@gmail.com>
2023-10-04 15:05:20 -0300

    class Tarefa

--- src/programa/Tarefa.java
+++ src/programa/Tarefa.java
@@ -1,4 +1,41 @@
+package programa;
 
-public class Tarefa {
 
-}
+	public class Tarefa {
+	    private String titulo;
+	    private String descricao;
+	    private String dataVencimento;
+
+	    public Tarefa(String titulo, String descricao, String dataVencimento) {
+	        this.titulo = titulo;
+	        this.descricao = descricao;
+	        this.dataVencimento = dataVencimento;
+	    }
+
+	    // Getters e setters
+
+	    public String getTitulo() {
+	        return titulo;
+	    }
+
+	    public void setTitulo(String titulo) {
+	        this.titulo = titulo;
+	    }
+
+	    public String getDescricao() {
+	        return descricao;
+	    }
+
+	    public void setDescricao(String descricao) {
+	        this.descricao = descricao;
+	    }
+
+	    public String getDataVencimento() {
+	        return dataVencimento;
+	    }
+
+	    public void setDataVencimento(String dataVencimento) {
+	        this.dataVencimento = dataVencimento;
+	    }
+	}
+
