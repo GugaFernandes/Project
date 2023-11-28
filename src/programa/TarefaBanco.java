f78329d4a88949edeca52c564d9d6b8cb034ffc6
Gustavo Germano Fernandes <gusgermes@gmail.com>
2023-11-28 19:22:40 -0300

    não mata a gente brunaaaaa

--- src/programa/TarefaBanco.java
+++ src/programa/TarefaBanco.java
@@ -1,5 +1,22 @@
 package programa;
 
-public class TarefaBanco {
 
+import java.util.ArrayList;
+
+
+public class TarefaBanco{
+    private static TarefaBanco instance;
+    private ArrayList<Tarefa> tarefas;
+    private TarefaBanco(){
+        tarefas = new ArrayList<>();
+    }
+    public static TarefaBanco getInstance(){
+        if (instance==null){
+            instance = new TarefaBanco();
+        }
+        return instance;
+    }
+    public ArrayList<Tarefa> getTarefas(){
+        return tarefas;
+    }
 }
