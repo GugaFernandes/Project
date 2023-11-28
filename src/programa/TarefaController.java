72bdace1acb7fd4290a37bde326f84e677466295
Gustavo Germano Fernandes <gusgermes@gmail.com>
2023-11-28 18:41:53 -0300

    the armagedon is comming

--- src/programa/TarefaController.java
+++ src/programa/TarefaController.java
@@ -1,5 +1,44 @@
 package programa;
 
+import java.util.ArrayList;
+
 public class TarefaController {
 
+	
+	
+    private TarefaDAO tarefaDAO;
+
+    public TarefaController(){
+        this.tarefaDAO = new TarefaDAO();
+    }
+
+    public void criarTarefa(String titulo, String descricao, String dataVencimento){
+        Tarefa tarefa = new Tarefa(titulo, descricao, dataVencimento);
+        tarefaDAO.criarTarefa(tarefa);
+    }
+
+    public void removerTarefa(String titulo){
+        tarefaDAO.removerTarefa(titulo);
+    }
+
+    public ArrayList<Tarefa> listarTarefas(){
+        return tarefaDAO.listarTarefas();
+    }
+
+    public Tarefa buscarTarefaPorTitulo(String titulo){
+        return tarefaDAO.buscarTarefaPorTitulo(titulo);
+   }
+
+    public void atualizarTarefa(Tarefa tarefa, String novaDescricao, String novaDataVencimento){
+        tarefaDAO.atualizarTarefa(tarefa, novaDescricao, novaDataVencimento);
+    }
 }
+	
+	
+	
+	
+	
+	
+	
+
+
